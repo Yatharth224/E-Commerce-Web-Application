333c3185153875569faf320bc2ef2c99191abb2b
Your Name <yatharthchouhan22@gmail.com>
2026-04-24 23:32:51 +0530

    views

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from store.models import Product  
-from django.db.models import Q,F
+
+
+from django.shortcuts import render  
+from django.db.models.aggregates import Count, Min, Max, Avg, Sum
+from store.models import  Product  ,Customer
+  
+
 
 
 
 def say_hello(request):
-    product = Product.objects.values('id','title','collection__title   ')
+
+    queryset = Customer.objects.annotate(is_new=True)
+     
     
-    return render (request, 'hello.html',{'name':'Yatharth', 'products': (product) })
+
+
+
+
+    return render(request, 'hello.html', {'name': 'Yatharth', 'result': list(queryset)})
