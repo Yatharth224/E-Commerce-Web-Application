409fd48af31084202b09a285cb6cd68ca4b999b2
Your Name <yatharthchouhan22@gmail.com>
2026-05-04 23:49:25 +0530

    views

--- playground/views.py
+++ playground/views.py
@@ -30,11 +30,8 @@ def say_hello(request):
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
+
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/list.html', {'products': products})
 
-
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    return render(request, 'products/detail.html', {'product': product})
