bcbda44d2e1c0add158b89c0d76cef9217828a26
Your Name <yatharthchouhan22@gmail.com>
2026-05-14 23:36:04 +0530

    views

--- playground/views.py
+++ playground/views.py
@@ -81,3 +81,6 @@ def add_to_cart(request, cart_id, product_id):
         cart_item.quantity += 1
         cart_item.save()
     return JsonResponse({'message': 'Added to cart'})
+
+
+from .models import Order, OrderItem
