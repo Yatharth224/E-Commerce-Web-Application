028be236b93b5ca7044100e3abe6e09ee74079e0
Your Name <yatharthchouhan22@gmail.com>
2026-05-15 23:39:06 +0530

    views

--- playground/views.py
+++ playground/views.py
@@ -93,3 +93,4 @@ def create_order(request, customer_id):
 from .models import Customer
 def customer_list(request):
     customers = Customer.objects.all()    
+return render(request, 'customers/list.html', {'customers': customers})
