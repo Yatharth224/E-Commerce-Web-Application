26c67d5503e766f88b5eb03271d665bd1df59175
Your Name <yatharthchouhan22@gmail.com>
2026-05-25 23:36:50 +0530

    urls

--- playground/urls.py
+++ playground/urls.py
@@ -12,8 +12,9 @@ urlpatterns = [
     path('products/<int:id>/', views.product_detail, name='product-detail'),
 
     path('collections/', views.collection_list, name='collection-list'),
-    
+
     path('collections/<int:id>/', views.collection_detail, name='collection-detail'),
+    
     path('carts/create/', views.create_cart, name='create-cart'),
     path('orders/create/<int:customer_id>/', views.create_order, name='create-order'),
     path('carts/<int:cart_id>/add/<int:product_id>/', views.add_to_cart, name='add-to-cart'),
