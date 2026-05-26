c5208c96458b87dde920f66aafa602437de32840
Your Name <yatharthchouhan22@gmail.com>
2026-05-26 23:26:41 +0530

    urls

--- playground/urls.py
+++ playground/urls.py
@@ -6,7 +6,6 @@ from. import views
 
 
 urlpatterns = [
-   
     path('products/', views.product_list, name='product-list'),
 
     path('products/<int:id>/', views.product_detail, name='product-detail'),
